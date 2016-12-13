8d844564afb350c2957f1924a3a32d342319cc26
Oscar Oliver <oscaroliver@iesmontsia.org>
2016-12-13 17:53:27 +0100

    Oscar

---
+++ src/Autor.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alumne
+ * Date: 13/12/16
+ * Time: 17:30
+ */
+class Autor
+{
+    private $nom;
+    private $cognom;
+    private $email;
+    private $telefon;
+    private $pais;
+
+    function __construct()
+    {
+
+    }
+}
