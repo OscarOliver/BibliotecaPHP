eb2a070b64eae797cbfcffba4b640e28773b30ab
Oscar Oliver <oscaroliver@iesmontsia.org>
2016-12-14 23:38:13 +0100

    Mostrar usuaris guardats

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+<table width="85%">
+    <tr><td><h1>Biblioteca</h1></td></tr>
+    <tr>
+        <td width="20%" >
+            <a href="formulariNouUsuari.html"><button value="Afegir usuari">Afegir usuari</button></a>
+        </td>
+        <td width="80%" height="500px">
+            <?php include "mostrarUsuaris.php"; ?>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
---
+++ public/mostrarUsuaris.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alumne
+ * Date: 14/12/16
+ * Time: 20:45
+ */
+require_once ("../src/DBConnection.php");
+$link = DBConnection::getConnection();
+
+// Comprovar la connexió, si no pot connectar-se donara error
+if ($link === false) {
+    die("ERROR: No s'ha pogut connectar. " . mysqli_connect_error());
+}
+else {
+    echo "<script>console.log( 'Connected successfully.' );</script>";
+}
+
+$sql = "SELECT * FROM usuari";
+
+$results = mysqli_query($link, $sql);
+
+echo "<table>";
+echo "<thead><tr>";
+echo    "<th>Id</th>";
+echo    "<th>Nom</th>";
+echo    "<th>Cognoms</th>";
+echo    "<th>Data naixement</th>";
+echo "</tr></thead>";
+echo "<tbody>";
+while ($row = $results->fetch_array()) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['nom'] . "</td>";
+    echo "<td>" . $row['cognom'] . "</td>";
+    echo "<td>" . $row['dataNaixement'] . "</td>";
+    echo "</tr>";
+}
+echo "</tbody></table>";
+
+$link->close();
+?>
