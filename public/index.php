36cc561feb9cc594c631355dfde42707546b2bd9
atcastells <aaroncastells@gmail.com>
2016-12-18 19:54:33 +0100

    Formulari en format PHP
    Afegit header a diferents arxius

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
     <tr><td><h1>Biblioteca</h1></td></tr>
     <tr>
         <td width="20%" >
-            <a href="formulariNouUsuari.html"><button value="Afegir usuari">Afegir usuari</button></a>
+            <a href="formulariNouUsuari.php"><button value="Afegir usuari">Afegir usuari</button></a>
         </td>
         <td width="80%" height="500px">
             <?php include "mostrarUsuaris.php"; ?>
