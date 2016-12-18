32f0a66b772834ff89d559ce106c7cb7184797e8
atcastells <aaroncastells@gmail.com>
2016-12-18 11:09:15 +0100

    + Formulari retornar llibre + Formulari prestar llibre

--- public/mostrarCataleg.php
+++ public/mostrarCataleg.php
@@ -1,25 +1,49 @@
 <?php
 require_once "../src/Cataleg.php";
+require_once "../src/Prestecs.php";
 
 $cataleg = new Cataleg();
+$prestecs = new Prestecs();
 
 $results = $cataleg ->get();
+$resultsPrestecs = $prestecs ->get();
 
-echo "<table>";
-echo "<thead>";
-echo "<tr><th colspan='4'>Cataleg de llibres</th></tr>";
-echo "<tr><th>Cataleg ID</th><th>Llibre ID</th><th>Prestar</th><th>Tornar</th></tr>";
-echo "</thead>";
-echo "<tbody>";
 
-//Loop per al contingut
+/*Llista de llibres per Prestar*/
 
+echo "<form action='prestarLlibre.php' method='post' autocomplete='off'>";
+echo "<label>Llibres disponibles</label>";
+echo "<br />";
+echo "<input list='disponibles' name='idCataleg' placeholder='Escriu el id del cataleg'>";
+echo "<datalist id='disponibles'>";
 while ($row = $results->fetch_array()){
-    echo "<tr>";
-    echo "<td>". $row['id'] ."</td>";
-    echo "<td>". $row['idLlibre'] ."</td>";
-    echo "<td> funcioPerPrestar </td>";
-    echo "<td> funcioPerTornar </td>";
-}
+$idCataleg = $row['id'];
+$disponible = true;
+    while ($rowPrestecs = $resultsPrestecs->fetch_array()){
+        if($rowPrestecs['idCataleg'] == $idCataleg && $rowPrestecs['dataDevolucio'] == null) $disponible = false;
+    }
 
-echo "</tbody></table>";
+    if($disponible)echo '<option value="'.$row[id].'">';
+}
+echo "</datalist>";
+echo "</input>";
+echo "<input type='submit'>";
+echo "</form>";
+
+/*Llista de llibres per retornar*/
+
+echo "<form action='retornarLlibre.php' method='post' autocomplete='off'>";
+echo "<label>Llibres a retornar</label>";
+echo "<br />";
+echo "<input list='retornar' name='idCataleg' placeholder='Escriu el id del cataleg'>";
+echo "<datalist id='retornar'>";
+while ($rowPrestecs = $resultsPrestecs->fetch_array()){
+    $idCataleg = $rowPrestecs['idCataleg'];
+    $valid = false;
+    if($rowPrestecs['dataDevolucio'] == null)$valid = true;
+    if ($valid) echo "<option value = '.$rowPrestecs[idCataleg].'>";
+}
+echo "</datalist>";
+echo "</input>";
+echo "<input type='submit'>";
+echo "</form>";
