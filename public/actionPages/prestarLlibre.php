dc903a8680548ec628661964fe3abd449b437a21
atcastells <aron.deltebre@gmail.com>
2017-01-07 16:19:08 +0100

    + Colors taules + Fix prestecs

--- public/actionPages/prestarLlibre.php
+++ public/actionPages/prestarLlibre.php
@@ -16,10 +16,12 @@ $idLlibre = Cataleg::getLlibre($id);
 $idLlibre = $idLlibre['idLlibre'];
 $usuaris = Usuari::getUsuaris();
 $invalidUsers = Prestecs::getUsuarisLimit();
+$dataDevolucio = date('Y-m-d H:i:s', strtotime('+1 month'));
 
 /*Llistat de usuaris*/
 echo "<form action='prestarLlibre.php' method='post'>
 <input type='hidden' name='idCataleg' value='" .$id."'>
+<input type='hidden' name='dataDevolucio' value='".$dataDevolucio."'>
 <input list='usuaris' name='usuari'>
 <datalist id='usuaris'>";
 while ($row = $usuaris ->fetch_array()){
@@ -36,7 +38,10 @@ if(sizeof($_POST) > 1){
     /*Seleccionem el id de usuari a partir del nom*/
     $usuari = filter_var($_POST['usuari'], FILTER_SANITIZE_NUMBER_INT);
 
+    /*recuperem la data de devolució*/
+
+    $dataDev = $_POST['dataDevolucio'];
     /*Realitzem el prestec*/
-    Prestecs::prestar($usuari,$id);
+    echo Prestecs::prestar($usuari,$id,$dataDev);
     header('Location: ../mostrarCataleg.php');
 }
